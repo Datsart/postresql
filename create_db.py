2ab54d371362ae3dd5ecaa6d452663312d60f5e0
artemdatsenko <dacenko.aa@yandex.ru>
2023-12-12 15:49:43 +0300

    readme

--- create_db.py
+++ create_db.py
@@ -2,12 +2,19 @@ from sqlalchemy import create_engine, MetaData, Table, Column, Integer, DateTime
 import psycopg2 # для коннекта
 from datetime import datetime
 
+name_db = str(input('Введите название БД: '))
+dialect = str(input('Введите диалект: '))
+user = str(input('Введите юзера: '))
+password = str(input('Введите пароль: '))
+host = str(input('Введите хост: '))
+port = str(input('Введите порт: '))
+
 # Строка подключения к базе данных
-db_url = 'postgresql://artemdatsenko:19980723@localhost:5432/log_info' # для движка
+db_url = f'{dialect}://{user}:{password}@{host}:{port}/{name_db}' # для движка
 
 # Устанавливаем соединение с базой данных PostgreSQL
 conn = psycopg2.connect(
-    database="log_info", user='artemdatsenko', password='19980723', host='localhost', port='5432'  # для курсора
+    database=f'{name_db}', user=f'{user}', password=f'{password}', host=f'{host}', port=f'{port}'  # для курсора
 )
 conn.autocommit = True  # Устанавливаем автоматический режим подтверждения транзакций
 
@@ -29,5 +36,14 @@ connection = engine.connect()
 print("Подключение успешно установлено.")
 
 # Создаем таблицу 'api_data' в базе данных
-metadata.create_all(engine)
+# metadata.create_all(engine)
 
+cursor = conn.cursor()
+cursor.execute("SELECT EXISTS(SELECT 1 FROM pg_database WHERE datname = 'log_info')")
+exists = cursor.fetchone()[0] # здесь True или False
+print(exists)
+
+if exists is not True:
+    name_db = str(input('Введите название БД: '))
+    cursor.execute(f'CREATE DATABASE {name_db}')
+    print(f'Создана база данных {name_db}')
