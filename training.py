a7b9da90fb4058a8c49d77c77a72177133e95ad0
artemdatsenko <dacenko.aa@yandex.ru>
2024-01-09 14:03:53 +0300

    8.2

--- training.py
+++ training.py
@@ -38,57 +38,56 @@ def generate_data(size):
 
 
 def result_metrics(*args, **kwargs):
-    df = pd.DataFrame(generate_data(size))
-    value_time = 0
-    ################################ ПЕРОЕ ЗАДАНИЕ
-    unique_countries = len(df['country'].unique())  # Количество уникальных стран в выборке
-    time.sleep(value_time)
-    ################################ ВТОРОЕ ЗАДАНИЕ
-    deposits_sum = df.groupby('country')['deposit'].sum()  # группируем страны
-    country_max_deposits = deposits_sum.idxmax()  # страна где больше всего депозитов
-    time.sleep(value_time)
-    ################################ ТРЕТЬЕ ЗАДАНИЕ
-    email_count = df.groupby('id')['email'].nunique()
-
-    clients_with_emails = email_count[
-        email_count > 1]  # фильтруем только тех клиентов, у которых больше 1 email
-
-    # получаем количество клиентов с более чем 1 email
-    clients_count_email = len(clients_with_emails)
-    time.sleep(value_time)
-    ################################ ЧЕТВЕРТОЕ ЗАДАНИЕ
-    country_count = df.groupby('id')['country'].nunique()
-
-    clients_countries = country_count[country_count > 1]
-
-    clients_count_countries = len(clients_countries)
-
-    ################################  ПЯТОЕ ЗАДАНИЕ
-    df['expenses_ratio'] = df['costs'] / df['deposit']  # создали новый столбец
-
-    max_ratio_client = df.loc[df['expenses_ratio'].idxmax()]  # достали клиента с max
-
-    email_max_ratio_client = max_ratio_client['email']
-    time.sleep(value_time)
-    name_metrics = ['Количество уникальных стран в выборке',
-                    'Страна в которой больше всего осталось депозитов у клиентов',
-                    'Количество клиентов у которых больше 1 email в выборке',
-                    'Количество клиентов  которые присутствуют больше чем в 1 стране',
-                    'Email клиента с максимальной долей трат от депозита']
-    value_metrics = [unique_countries,
-                     country_max_deposits,
-                     clients_count_email,
-                     clients_count_countries,
-                     email_max_ratio_client]
     counter = 0
+    for j in range(int(size) * 5):  # 5 - количество метрик
+        df = pd.DataFrame(generate_data(size))
+        value_time = 0
+        ################################ ПЕРОЕ ЗАДАНИЕ
+        unique_countries = len(df['country'].unique())  # Количество уникальных стран в выборке
+        time.sleep(value_time)
+        ################################ ВТОРОЕ ЗАДАНИЕ
+        deposits_sum = df.groupby('country')['deposit'].sum()  # группируем страны
+        country_max_deposits = deposits_sum.idxmax()  # страна где больше всего депозитов
+        time.sleep(value_time)
+        ################################ ТРЕТЬЕ ЗАДАНИЕ
+        email_count = df.groupby('id')['email'].nunique()
 
-    date = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
-    hash_date = hashlib.md5(date.encode())
-    if 'date_term' and 'hash_date_term' in locals():
-        date = date_term
+        clients_with_emails = email_count[
+            email_count > 1]  # фильтруем только тех клиентов, у которых больше 1 email
+
+        # получаем количество клиентов с более чем 1 email
+        clients_count_email = len(clients_with_emails)
+        time.sleep(value_time)
+        ################################ ЧЕТВЕРТОЕ ЗАДАНИЕ
+        country_count = df.groupby('id')['country'].nunique()
+
+        clients_countries = country_count[country_count > 1]
+
+        clients_count_countries = len(clients_countries)
+
+        ################################  ПЯТОЕ ЗАДАНИЕ
+        df['expenses_ratio'] = df['costs'] / df['deposit']  # создали новый столбец
+
+        max_ratio_client = df.loc[df['expenses_ratio'].idxmax()]  # достали клиента с max
+
+        email_max_ratio_client = max_ratio_client['email']
+        time.sleep(value_time)
+        name_metrics = ['Количество уникальных стран в выборке',
+                        'Страна в которой больше всего осталось депозитов у клиентов',
+                        'Количество клиентов у которых больше 1 email в выборке',
+                        'Количество клиентов  которые присутствуют больше чем в 1 стране',
+                        'Email клиента с максимальной долей трат от депозита']
+        value_metrics = [unique_countries,
+                         country_max_deposits,
+                         clients_count_email,
+                         clients_count_countries,
+                         email_max_ratio_client]
+
+        date = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
         hash_date = hashlib.md5(date.encode())
-    for j in range(int(size) * len(name_metrics)):
-
+        if 'date_term' and 'hash_date_term' in locals():
+            date = date_term
+            hash_date = hashlib.md5(date.encode())
         df = pd.DataFrame({
             'hash': [hash_date.hexdigest()],
             'feature': [name_metrics[counter]],
@@ -114,4 +113,4 @@ try:
     result_metrics(size, date_term, hash_date_term)
 except IndexError:
     size = sys.argv[1]
-    result_metrics(size, None, None)
+    result_metrics(size)
