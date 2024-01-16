54e10e629439e45d5aa9db6300fdc41df5a31763
artemdatsenko <dacenko.aa@yandex.ru>
2024-01-16 14:45:58 +0300

    9.5 исправил get_stat.py функцию для отправка запроса закинул в test_response.py (для отправки раскомментить функцию в файле) поправил ТГ бота

--- get_stat.py
+++ get_stat.py
@@ -17,20 +17,5 @@ def get_info(hash_id):
         'datetime': result_info['datetime'].iloc[0]
     }
     res_json = json.dumps(data, ensure_ascii=False, indent=2)
-    print(res_json)
+    # print(res_json)
     return res_json
-
-
-def get_response_stat(hash_id):
-    url = "http://127.0.0.1:5050/get_stat"
-    headers = {
-        'Content-Type': 'application/json'
-    }
-    payload = get_info(hash_id)
-
-    response = requests.post(url, json=payload, headers=headers)
-    return f'{response.json()}'
-
-
-hash_id = '0c476f060ddc927f5145b0b4a5ff7fa9'
-# print(get_response_stat(hash_id))
